2999c3a88ed5489d66d0621d3542e18caa1774c0
nhnam <37695601+hoai97nam@users.noreply.github.com>
2022-11-19 22:26:46 +0700

    push code

--- server/database.ts
+++ server/database.ts
@@ -1,15 +1,27 @@
 
 import * as _ from 'lodash';
-import {LESSONS} from "./database-data";
+import {LESSONS, USERS} from "./database-data";
+import { DbUser } from './db-user';
 
 
 class InMemoryDatabase {
 
+    userCounter = 0;
     readAllLessons() {
         return _.values(LESSONS);
     }
 
+    createUser(email: string, passwordDigest: string) {
+        const id = this.userCounter++;
+        const user: DbUser = {
+            id,
+            email, 
+            passwordDigest
+        }
 
+        USERS[id] = user;
+        return user;
+    }
 }
 
 export const db = new InMemoryDatabase();
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,24 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AuthService } from '../services/auth.service';
 
 @Component({
-  selector: 'signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css', '../common/forms.css']
+    selector: 'signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.css', '../common/forms.css']
 })
 export class SignupComponent implements OnInit {
 
-    form:FormGroup;
+    form: FormGroup;
+    errors: string[] = [];
+    messagePerErrorCode = {
+        min: 'Minimum length 10',
+        uppercase: 'Must have uppercase letters',
+        digits: 'Must have at least 2 digits'
+    }
 
-    constructor(private fb: FormBuilder) {
+    constructor(private fb: FormBuilder, private authService: AuthService) {
 
         this.form = this.fb.group({
-            email: ['',Validators.required],
-            password: ['',Validators.required],
-            confirm: ['',Validators.required]
+            email: ['', Validators.required],
+            password: ['', Validators.required],
+            confirm: ['', Validators.required]
         });
-
-
     }
 
     ngOnInit() {
@@ -28,8 +33,13 @@ export class SignupComponent implements OnInit {
 
     signUp() {
         const val = this.form.value;
-
-        //TODO
+        if (val.email && val.password && val.confirm) {
+            this.authService.signUp(val.user, val.password)
+                .subscribe(
+                    () => console.log('user created successfully'),
+                    response => this.errors = response.error.errors
+                )
+        }
 
     }
 
